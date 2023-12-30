34c26912c67ec450fd213a23f00c69876a610b46
Bada-Don <harshitashwani@gmail.com>
2023-12-31 00:52:11 +0530

    Carousel Projects

--- My Porfolio.js
+++ My Porfolio.js
@@ -2,3 +2,26 @@ var typed = new Typed('#element', {
     strings: ['coding."', 'web development."', 'designing."', 'gaming."','music listening."'],
     typeSpeed: 50,
   });
+
+    // Function to update the class based on screen size
+    function updateCarouselWidth() {
+        var carousel = document.getElementById('carouselExampleIndicators');
+
+        // Check the screen width
+        if (window.innerWidth < 500) {
+            // If the screen width is less than 768 pixels, set a new class for mobile
+            carousel.classList.remove('w-50');
+            carousel.classList.add('w-100');
+        } else if(window.innerWidth > 500) {
+            // Otherwise, set a different class for desktop
+            carousel.classList.remove('w-100');
+            carousel.classList.add('w-50');
+        }
+    }
+
+    // Call the function initially to set the class based on the initial screen size
+    updateCarouselWidth();
+
+    // Attach the function to the window resize event to update the class dynamically
+    window.addEventListener('resize', updateCarouselWidth);
+
